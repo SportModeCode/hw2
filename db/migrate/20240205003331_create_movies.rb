1cd6b6f640acafa7a9b8c5b142a18a8f0192a2eb
SportMode Code <james.epperson@kellogg.northwestern.edu>
2024-02-05 01:09:03 +0000

    progress, models created and migrated

--- db/migrate/20240205003331_create_movies.rb
+++ db/migrate/20240205003331_create_movies.rb
@@ -2,10 +2,10 @@ class CreateMovies < ActiveRecord::Migration[7.1]
   def change
     create_table :movies do |t|
       t.string "name"
-      t.string "year"
+      t.integer "year_released"
       t.string "rated"
       t.integer "studio_id" # foreign key
-      
+
       t.timestamps
     end
   end
